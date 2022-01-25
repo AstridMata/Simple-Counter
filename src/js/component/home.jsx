9997f670cbe4aea88250fd879ca2296637620778
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-25 01:27:26 +0000

    solo el contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./Card.jsx";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props);
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<div className="row">
+					<div className="col">{props.sec}</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+					<div className="col">
+						<Card />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,10 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+let sec = 0;
+setInterval(() => {
+	sec += 1;
+	ReactDOM.render(<Home sec={sec} />, document.querySelector("#app"));
+}, 1000);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
