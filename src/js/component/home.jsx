fd85540a14c1846feef5e3499429d2044bc1f340
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-26 01:44:18 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,27 +3,11 @@ import Card from "./Card.jsx";
 
 //create your first component
 const Home = (props) => {
-	console.log(props);
 	return (
 		<div>
 			<div className="container">
 				<div className="row">
-					<div className="col">{props.sec}</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
+					<div className="col">{props.digitOne}</div>
 					<div className="col">
 						<Card />
 					</div>
--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,17 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Card from "./component/Card.jsx";
+
 let sec = 0;
 setInterval(() => {
+	const four = Math.floor(sec / 1000);
+	const three = Math.floor(sec / 100);
+	const two = Math.floor(sec / 10);
+	const one = Math.floor(sec / 1);
+	console.log(four, three, two, one);
 	sec += 1;
-	ReactDOM.render(<Home sec={sec} />, document.querySelector("#app"));
+	ReactDOM.render(<Home digitOne={sec} />, document.querySelector("#app"));
 }, 1000);
 
 //render your react application
